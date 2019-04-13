320063c339c94b5c667deeb2bbb5a1878bc21b36
Mr_Goldberg <gold@berg>
2019-04-13 12:51:54 -0400

    Fix building on latest vspkg protobuf package.

--- build_set_protobuf_directories.bat
+++ build_set_protobuf_directories.bat
@@ -2,3 +2,9 @@ SET PROTOBUF_X86_DIRECTORY=..\vcpkg\packages\protobuf_x86-windows-static
 SET PROTOBUF_X64_DIRECTORY=..\vcpkg\packages\protobuf_x64-windows-static
 rem location of protoc in protobuf directories:
 SET PROTOC_DIRECTORY=\tools\protobuf\protoc.exe
+
+if exist "%PROTOBUF_X64_DIRECTORY%\lib\libprotobuf-lite.lib" (
+SET PROTOBUF_LIBRARY=\lib\libprotobuf-lite.lib
+) else (
+SET PROTOBUF_LIBRARY=\lib\libprotobuf.lib
+)
