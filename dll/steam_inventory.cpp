2a659043d3124172596c51076c7e1c8f33d759ea
Mr_Goldberg <gold@berg>
2019-07-17 13:54:23 -0400

    Fix build.

--- dll/steam_inventory.cpp
+++
@@ -1,5 +0,0 @@
-#include "steam_inventory.h"
-
-std::once_flag   Steam_Inventory::items_loading;
-std::atomic_bool Steam_Inventory::items_loaded(false);
-std::map<SteamItemDef_t, std::map<std::string, std::string>> Steam_Inventory::items;
