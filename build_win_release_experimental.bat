9729ae9e4de70b4400ba0f8f0216fa84b5311444
Nemirtingas <nanaki89@hotmail.fr>
2019-08-14 23:01:56 +0200

    Added overlay files to cmake

--- build_win_release_experimental.bat
+++ build_win_release_experimental.bat
@@ -9,6 +9,6 @@ cl /LD /DEMU_RELEASE_BUILD /DEMU_EXPERIMENTAL_BUILD /DNDEBUG steamclient.cpp /EH
 %PROTOBUF_X64_DIRECTORY%%PROTOC_DIRECTORY% -I.\dll\ --cpp_out=.\dll\ .\dll\net.proto
 call build_env_x64.bat
 cl dll/rtlgenrandom.c dll/rtlgenrandom.def
-cl /LD /DEMU_RELEASE_BUILD /DEMU_EXPERIMENTAL_BUILD /DNDEBUG /IImGui /Iglew\include /I%PROTOBUF_X64_DIRECTORY%\include\ dll/*.cpp dll/*.cc detours/*.cpp ImGui/*.cpp ImGui/impls/*.cpp overlay_experimental/*.cpp %PROTOBUF_X64_DIRECTORY%%PROTOBUF_LIBRARY% /LIBPATH:"glew\lib\Release\x64\" Iphlpapi.lib Ws2_32.lib rtlgenrandom.lib Shell32.lib glew32s.lib/EHsc /MP12 /Ox /link /debug:none /OUT:release\experimental\steam_api64.dll
+cl /LD /DEMU_RELEASE_BUILD /DEMU_EXPERIMENTAL_BUILD /DNDEBUG /IImGui /Iglew\include /I%PROTOBUF_X64_DIRECTORY%\include\ dll/*.cpp dll/*.cc detours/*.cpp ImGui/*.cpp ImGui/impls/*.cpp overlay_experimental/*.cpp %PROTOBUF_X64_DIRECTORY%%PROTOBUF_LIBRARY% /LIBPATH:"glew\lib\Release\x64\" Iphlpapi.lib Ws2_32.lib rtlgenrandom.lib Shell32.lib glew32s.lib /EHsc /MP12 /Ox /link /debug:none /OUT:release\experimental\steam_api64.dll
 cl /LD /DEMU_RELEASE_BUILD /DEMU_EXPERIMENTAL_BUILD /DNDEBUG steamclient.cpp /EHsc /MP12 /Ox /link /OUT:release\experimental\steamclient64.dll
 copy Readme_experimental.txt release\experimental\Readme.txt
