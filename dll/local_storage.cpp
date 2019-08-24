abb26d402fbb8ce349bf4ec0d2bfb53758c0f06d
Nemirtingas <nanaki89@hotmail.fr>
2019-08-24 18:10:56 +0200

    Fix

--- dll/local_storage.cpp
+++ dll/local_storage.cpp
@@ -135,7 +135,7 @@ bool Local_Storage::update_save_filenames(std::string folder)
 
 bool Local_Storage::load_inventory_file(nlohmann::json& json, std::string const&file)
 {
-    return true
+    return true;
 }
 
 bool Local_Storage::write_inventory_file(nlohmann::json const& json, std::string const&file)
