24fbb0978c82d3d7e407845abeadefac420a8af9
Mr_Goldberg <gold@berg>
2020-04-29 10:14:02 -0400

    Allow building with the .bat using vs build tools.

--- build_env_x64.bat
+++ build_env_x64.bat
@@ -7,6 +7,8 @@ set VS_Base_Path=C:\Program Files (x86)
 if exist "%VS_Base_Path%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" goto vs2019
 if exist "%VS_Base_Path%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" goto vs2017
 if exist "%VS_Base_Path%\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" goto vs14
+if exist "%VS_Base_Path%\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto vs2019_bt
+if exist "%VS_Base_Path%\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto vs2017_bt
 if exist ".\sdk_standalone\set_vars64.bat" goto gitlabci
 
 :vs2019
@@ -19,6 +21,14 @@ goto batend
 
 :vs14
 call "%VS_Base_Path%\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat"
+goto batend
+
+:vs2019_bt
+call "%VS_Base_Path%\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+goto batend
+
+:vs2017_bt
+call "%VS_Base_Path%\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 goto batend
 
 :gitlabci
